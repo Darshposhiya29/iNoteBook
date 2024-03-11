188850e248fbff8cb104ef9545b8fc53150f9bc6
Darshposhiya29 <darsh.pict21@sot.pdpu.ac.in>
2024-03-11 14:49:52 +0530

    css commit1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import './Login.css';
 
 export const Login = (props) => {
     const [credentials, setCredentials] = useState({ email: "", password: "" })
